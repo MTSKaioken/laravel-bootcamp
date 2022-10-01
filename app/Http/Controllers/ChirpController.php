f66c2cb3af43bc6abe2a305dbc75f55c1648e113
MTSKaioken <kaio.martins2@aluno.unip.br>
2022-10-01 19:45:05 -0300

    Index Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChirpController extends Controller
 {
@@ -15,6 +16,9 @@ class ChirpController extends Controller
     public function index()
     {
         //
+        return Inertia::render('Chirps/Index', [
+            //
+        ]);
     }
 
     /**
